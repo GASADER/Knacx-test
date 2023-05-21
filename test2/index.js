9e67b14d043a34bcaf302012615df7bbf4abc306
GASADER <kajorhkaen@gmail.com>
2023-05-21 11:28:45 +0700

    Test2-Feat: add validate

--- test2/index.js
+++ test2/index.js
@@ -1,6 +1,28 @@
 const prompt = require ('prompt-sync')()
 
-run = () => {
-    console.log("hello world")
+// funtion validate มีค่า boolean
+ function vaildInput(input){
+    //รับ4ตัว
+    if(input.length !== 4){
+        return false
+    //เช็คตัวอักษร
+    } else if(/^[A-Za-z]+$/.test(input)){
+        return false
+    }
+    else{
+        return true
+    }
 }
+
+
+//
+run = () => {
+    //รับค่าจากprompt
+    const input = prompt('Enter a 4 letter word:')
+    //เช็คvalidate
+    if(!vaildInput(input)){
+        console.log("Please enter a valid 4 letter word")
+    }
+}
+//สั่งrun program
 run()
