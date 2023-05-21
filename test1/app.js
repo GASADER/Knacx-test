006436a9d5dcb0c05b078b7fb40d0e69cfcfa4cf
GASADER <kajorhkaen@gmail.com>
2023-05-21 16:27:18 +0700

    Test1-Add:Analysis setting server and connect DB

---
+++ test1/app.js
@@ -0,0 +1,31 @@
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+
+import connectionDB from "./config/sqlConfig.js";
+
+const ipAddress = process.env.API_IPADDRESS;
+const port = process.env.API_PORT;
+const app = express();
+
+const corsOptions = {
+    origin: "http://localhost:3000",
+    credentials: true,
+  };
+
+async function run() {
+    
+  app.use(cors(corsOptions));
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({extended: true }));
+
+  await connectionDB();
+
+   await app.listen(port, ipAddress, () => {
+    console.log(
+      `Web Application Server is running on ${ipAddress} port ${port}`
+    );
+    console.log(`Address: http://${ipAddress}:${port}`);
+  });
+}
+run()
